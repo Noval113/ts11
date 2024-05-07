b57faa0e6bc2dab70d8963432f4e1e67095e2111
Naufal113 <169121712+Noval113@users.noreply.github.com>
2024-05-07 12:17:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,14 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# reading the database
 data = pd.read_csv("tips.csv")
 
-# Scatter plot with day against tip
+st.subheader("Scatter Plot")
 plt.scatter(data['day'], data['tip'], c=data['size'], 
 			s=data['total_bill'])
 
-# Adding Title to the Plot
 plt.title("Scatter")
 
-# Setting the X and Y labels
 plt.xlabel('Day')
 plt.ylabel('Tip')
 
